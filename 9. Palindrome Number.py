71ac90897cae387f7baeeda077ffe019815c0ffa
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-06-22 21:15:55 +0500

    Add files via upload

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        # Numbers ending in 0 that are not 0 themselves are not palindromes
+        if x % 10 == 0 and x != 0:
+            return False
+        
+        reverted_number = 0
+        while x > reverted_number:
+            reverted_number = reverted_number * 10 + x % 10
+            x //= 10
+        
+        # When the length is odd, we can get rid of the middle digit by reverted_number // 10
+        return x == reverted_number or x == reverted_number // 10
