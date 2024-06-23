d497aa3a5b6a58c0f43d1a5cb15bb78a8b33819c
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-06-23 23:46:59 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -33,3 +33,8 @@ FROM Weather w
 JOIN Weather w_prev ON w.recordDate = DATE_ADD(w_prev.recordDate, INTERVAL 1 DAY)
 WHERE w.temperature > w_prev.temperature;
 
+-- 1683. Invalid Tweets
+SELECT tweet_id  
+FROM Tweets
+WHERE LENGTH(content)>15;
+
