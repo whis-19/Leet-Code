c6e3811084723b43cca22f27a853b891785ec5a2
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-06-23 22:23:14 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -14,3 +14,10 @@ SELECT e.name, eu.unique_id
 FROM Employees e
 LEFT JOIN EmployeeUNI eu ON e.id = eu.id
 ORDER BY e.id;
+
+-- 1581. Customer Who Visited but Did Not Make Any Transactions
+SELECT v.customer_id, COUNT(*) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
