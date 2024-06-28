0dbd249e949a87e5922db1aa11db116bd0480544
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-06-28 20:43:31 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -56,7 +56,7 @@ WHERE referee_id!=2 OR referee_id is NULL ;
 -- 1757. Recyclable and Low Fat Products
 SELECT product_id FROM Products WHERE low_fats='Y' AND recyclable ='Y';
 
-# 175. Combine Two Tables
+-- 175. Combine Two Tables
 SELECT 
     p.FirstName, p.LastName, a.City, a.State
 FROM 
@@ -66,3 +66,53 @@ LEFT JOIN
 ON 
     p.PersonId = a.PersonId;
 
+-- 176. Second Highest Salary
+SELECT 
+    MAX(Salary) AS SecondHighestSalary
+FROM 
+    Employee
+WHERE 
+    Salary < (SELECT MAX(Salary) FROM Employee);
+
+-- 177. Nth Highest Salary
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N-1;
+  RETURN (
+      SELECT DISTINCT(salary) from Employee order by salary DESC
+      LIMIT 1 OFFSET N
+      
+  );
+END
+
+-- 178. Rank Scores
+SELECT
+    Score, DENSE_RANK() OVER (ORDER BY Score DESC) AS "Rank"
+FROM
+    Scores
+ORDER BY
+    Score DESC;
+
+-- 262. Trips and Users
+SELECT
+    request_at AS day,
+    ROUND(SUM((status <> 'completed')) / COUNT(*), 2) AS "Cancellation Rate"
+FROM Trips
+WHERE
+    request_at BETWEEN '2013-10-01' AND '2013-10-03'
+    AND
+    (client_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes'))
+    AND
+    (driver_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes'))
+GROUP BY 1
+
+-- 550. Game Play Analysis IV
+SELECT
+  ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM
+  Activity
+WHERE
+  (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
+  IN (
+    SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id
+  )
