77587bef644ba9d2928cfd9f32d9067041b20f32
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-04 23:49:22 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -128,4 +128,7 @@ FROM employee AS a
 JOIN employee AS b ON a.managerId = b.id
 WHERE a.salary > b.salary;
 
+-- 182. Duplicate Emails
+SELECT DISTINCT(p1.email) from Person p1, Person p2
+where p1.id <> p2.id AND p1.email = p2.email;
 
