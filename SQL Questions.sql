74a0b6a7750cb5a3cbbba2dbaa4bf06ad311e325
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-04 23:49:51 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -138,3 +138,12 @@ FROM customers
 LEFT JOIN orders ON customers.id = orders.customerId
 WHERE orders.customerId IS NULL;
 
+-- 184. Department Highest Salary
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE (e.departmentId, e.salary) IN (
+    SELECT departmentId, MAX(salary)
+    FROM Employee
+    GROUP BY departmentId
+);
