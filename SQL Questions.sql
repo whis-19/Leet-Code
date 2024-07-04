03a4cb9543bb3570366fa422e0e23ac7938df93c
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-04 23:52:06 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -207,3 +207,10 @@ FROM Orders
     GROUP BY customer_number
     ORDER BY COUNT(*) DESC
 LIMIT 1
+
+-- 596. Classes More Than 5 Students
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5;
+
