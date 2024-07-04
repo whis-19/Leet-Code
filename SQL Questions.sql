573c93069f38674d0221a92cc595ebcfa87f5d5d
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-04 23:50:10 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -147,3 +147,20 @@ WHERE (e.departmentId, e.salary) IN (
     FROM Employee
     GROUP BY departmentId
 );
+
+-- 185. Department Top Three Salaries
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM
+    Employee e
+    JOIN Department d ON e.departmentId = d.id
+WHERE
+    (
+        SELECT COUNT(DISTINCT salary)
+        FROM Employee e2
+        WHERE e2.departmentId = e.departmentId AND e2.salary >= e.salary
+    ) <= 3
+ORDER BY
+    Department, Salary DESC;
