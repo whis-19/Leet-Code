0c213fd3723b91e3cee25e05eb2dea9f42327216
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-04 23:48:52 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -116,3 +116,9 @@ WHERE
   IN (
     SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id
   )
+
+-- 180. Consecutive Numbers
+SELECT DISTINCT L1.num  AS ConsecutiveNums 
+FROM Logs L1, Logs L2,Logs L3 
+WHERE L1.id=L2.id - 1 AND L1.num=L2.num and  L2.id=L3.id-1 and L2.num=L3.num;
+
