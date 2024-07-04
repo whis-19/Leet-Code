5fc100c43c7e60d11927adb0191a0c4353e01651
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-04 23:51:06 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -179,3 +179,7 @@ JOIN Employee b ON a.id = b.managerId
 GROUP BY b.managerId 
 HAVING COUNT(*) >= 5
 
+-- 577. Employee Bonus
+SELECT Employee.name,Bonus.bonus FROM Employee 
+LEFT JOIN Bonus ON Employee.empID = Bonus.empID
+WHERE bonus < 1000 OR Bonus IS NULL;
