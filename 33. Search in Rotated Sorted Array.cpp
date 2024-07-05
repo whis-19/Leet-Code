6235abba2efa4a293ee4b8ece9a54ca4e338b5f9
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-05 15:38:55 +0500

    Add files via upload

---
+++ 33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            // Check which part is sorted
+            if (nums[left] <= nums[mid]) { // Left half is sorted
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1; // Target is in the left sorted half
+                } else {
+                    left = mid + 1; // Target is in the right half
+                }
+            } else { // Right half is sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1; // Target is in the right sorted half
+                } else {
+                    right = mid - 1; // Target is in the left half
+                }
+            }
+        }
+        
+        return -1; // Target not found
+    }
+};
