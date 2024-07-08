594d624683adbb5af84553e431145cdbecceab73
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-08 15:42:36 +0500

    Add files via upload

---
+++ 143. Reorder List.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    void reorderList(ListNode *head)
+    {
+        if (!head || !head->next || !head->next->next)
+            return;
+
+        // Step 1: Find the middle of the list
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Step 2: Reverse the second half of the list
+        ListNode *prev = nullptr, *curr = slow->next, *next = nullptr;
+        slow->next = nullptr; // Break the list into two halves
+        while (curr)
+        {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        ListNode *second = prev; // Head of the reversed second half
+
+        // Step 3: Merge the two halves
+        ListNode *first = head;
+        while (second)
+        {
+            ListNode *temp1 = first->next;
+            ListNode *temp2 = second->next;
+
+            first->next = second;
+            second->next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+};
