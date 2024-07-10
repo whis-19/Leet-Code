482295d9fd1da540e4ea533f933aaf06f95ff185
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-10 10:33:53 +0500

    Add files via upload

---
+++ 1598. Crawler Log Folder.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def minOperations(self, logs):
+        depth = 0
+        for log in logs:
+            if log == "../":
+                if depth > 0:
+                    depth -= 1
+            elif log != "./":
+                depth += 1
+        return depth
