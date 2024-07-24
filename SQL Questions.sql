e019590c0d0e0b52267a867998d2d427ea40d89c
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-25 02:40:29 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -1,3 +1,8 @@
+-- 1193. Monthly Transactions I
+SELECT  SUBSTR(trans_date,1,7) as month, country, count(id) as trans_count, SUM(CASE WHEN state = 'approved' then 1 else 0 END) as approved_count, SUM(amount) as trans_total_amount, SUM(CASE WHEN state = 'approved' then amount else 0 END) as approved_total_amount
+FROM Transactions
+GROUP BY month, country
+
 -- 1204. Last Person to Fit in the Bus
 SELECT 
     q1.person_name
