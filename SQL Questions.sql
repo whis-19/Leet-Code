bfdaccb7914d917941e90e23e5d73deb562d0044
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-25 02:34:08 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -214,3 +214,19 @@ FROM Courses
 GROUP BY class
 HAVING COUNT(student) >= 5;
 
+-- 1341. Movie Rating
+(SELECT name AS results
+FROM MovieRating JOIN Users USING(user_id)
+GROUP BY name
+ORDER BY COUNT(*) DESC, name
+LIMIT 1)
+
+UNION ALL
+
+(SELECT title AS results
+FROM MovieRating JOIN Movies USING(movie_id)
+WHERE EXTRACT(YEAR_MONTH FROM created_at) = 202002
+GROUP BY title
+ORDER BY AVG(rating) DESC, title
+LIMIT 1);
+
