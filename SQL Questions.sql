8fc8733c425c7b25f7c2bce49bdc2c4686af574b
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-27 15:38:19 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -1,3 +1,12 @@
+-- 601. Human Traffic of Stadium
+SELECT s.id, s.visit_date, s.people FROM Stadium s, Stadium t, Stadium u
+WHERE ((s.id+1 = t.id AND s.id+2 = u.id) 
+    OR (s.id-1 = t.id AND s.id+1 = u.id) 
+    OR (s.id-1 = t.id AND s.id-2 = u.id))
+    AND s.people>=100 AND t.people>=100 AND u.people>=100
+GROUP BY s.id
+ORDER BY id
+    
 -- 1179. Reformat Department Table
 SELECT Department.id,
 sum(if(month='Jan',revenue,null)) as Jan_Revenue,
