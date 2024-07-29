4cbf2f3521668ec2c38f6bdd3c23537d6ef9d840
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-29 10:49:32 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -1,3 +1,10 @@
+-- 1158. Market Analysis I
+SELECT u.user_id as buyer_id, u.join_date, count(o.order_id) as 'orders_in_2019'
+FROM users u
+LEFT JOIN Orders o
+ON o.buyer_id=u.user_id AND YEAR(order_date)='2019'
+GROUP BY u.user_id
+
 -- 1164. Product Price at a Given Date
 select product_id ,10 as price 
 from products
