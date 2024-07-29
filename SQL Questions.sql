c71f2826f8f0ae647a211261450a94a04a7db1f0
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-07-29 10:46:49 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -1,3 +1,15 @@
+-- 1174. Immediate Food Delivery II
+select ROUND(SUM(IF(ct.o_date = ct.d_date,1,0))*100/(select COUNT(DISTINCT(customer_id)) from Delivery)
+,2) as immediate_percentage
+from
+(
+
+select customer_id, MIN(order_date) as o_date , MIN(customer_pref_delivery_date) as d_date from Delivery 
+group by customer_id
+order by customer_id
+
+) as ct
+
 -- 1321. Restaurant Growth
 # Write your MySQL query statement below
 SELECT
