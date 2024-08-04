e458bbbefcba3078a1489e4506789db2103d0015
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-08-04 11:41:41 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -1,3 +1,10 @@
+-- 1587. Bank Account Summary II
+SELECT u.name AS NAME,SUM(t.amount) AS BALANCE
+FROM Users u, Transactions t
+WHERE u.account=t.account
+GROUP BY u.account
+HAVING SUM(t.amount)>10000;
+
 -- 1527. Patients With a Condition
 SELECT * 
 FROM patients 
