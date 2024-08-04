18f6e14c65d195ee7bc181fdde389e3ed09b2bb9
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-08-04 11:42:27 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -1,3 +1,11 @@
+-- 1633. Percentage of Users Attended a Contest
+select 
+contest_id, 
+round(count(distinct user_id) * 100 /(select count(user_id) from Users) ,2) as percentage
+from  Register
+group by contest_id
+order by percentage desc,contest_id
+
 -- 1587. Bank Account Summary II
 SELECT u.name AS NAME,SUM(t.amount) AS BALANCE
 FROM Users u, Transactions t
