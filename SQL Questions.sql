377658d91ee1e4a5da4e0685c6ce53014da43dc8
Muhammad Abdullah <132306415+whis-19@users.noreply.github.com>
2024-08-06 15:35:50 +0500

    Update SQL Questions.sql

--- SQL Questions.sql
+++ SQL Questions.sql
@@ -1,3 +1,16 @@
+-- 1795. Rearrange Products Table
+SELECT product_id, 'store1' AS store, store1 AS price 
+FROM Products 
+WHERE store1 IS NOT NULL
+UNION 
+SELECT product_id, 'store2' AS store, store2 AS price 
+FROM Products 
+WHERE store2 IS NOT NULL
+UNION 
+SELECT product_id, 'store3' AS store, store3 AS price 
+FROM Products 
+WHERE store3 IS NOT NULL
+
 -- 1789. Primary Department for Each Employee
 SELECT employee_id, department_id
 FROM Employee
